abc214359272fa36650f75cd20ac8ebf96e804af
Sinan Taylan <sinan.taylan@outlook.com>
2018-11-01 14:03:11 +0300

    add react files

---
+++ JS/my-shop-01/src/Catalog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Catalog.css';
+
+class Catalog extends React.Component {
+  render() {
+    
+      return <div className="catalog-style">
+      <h2>Catalog</h2></div>
+  }
+}
+export default Catalog;
